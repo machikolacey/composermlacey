3c75f329be0e106a2d4b41eb9add60ad5a78d6ab
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-01 19:33:22 +0000

    Added static folder and base.css file

--- mlkshop/urls.py
+++ mlkshop/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
     path('', include('home.urls')),
 ]
 
