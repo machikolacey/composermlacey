c0df8f5d015322e7c93d933257f3041c24ed4bad
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-02 18:24:29 +0000

    Deployment steps - allowed hosts, etc

--- mlkshop/settings.py
+++ mlkshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['machiko-lacey-kimura-pianist.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['composermlacey.herokuapp.com', 'localhost']
 
 
 # Application definition
