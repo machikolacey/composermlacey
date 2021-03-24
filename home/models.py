26745f9c897ed1b82bf728319cb00454cf9f5e65
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-24 22:33:36 +0000

    Added Pep8 fixes on home/ admin.py

--- home/models.py
+++ home/models.py
@@ -8,13 +8,11 @@ class Advert(models.Model):
     link_url = models.URLField(max_length=1024, null=True, blank=True)
     banner = models.ImageField(null=True, blank=True)
     banner_tablet_size = models.ImageField(null=True, blank=True)
-    banner_mobile_size = models.ImageField(null=True, blank=True)    
+    banner_mobile_size = models.ImageField(null=True, blank=True)
     name = models.TextField(null=True, blank=True)
-
 
     def __str__(self):
         return self.name
-
 
 
 class homepageVideo(models.Model):
@@ -24,4 +22,6 @@ class homepageVideo(models.Model):
     youtube_link = models.CharField(max_length=254, null=True, blank=True)
     details = models.TextField(null=True, blank=True)
     recorded = models.CharField(max_length=254, null=True, blank=True)
-    
+
+    def __str__(self):
+        return self.title
