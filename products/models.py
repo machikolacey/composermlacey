cb8fb92fe7951d94d2778b1071304903b21420bf
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-27 21:39:35 +0000

    Fixed pep8 validation errors on products/models.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -9,7 +8,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, default='', blank=True)
 
     def __str__(self):
         return self.name
@@ -21,21 +20,21 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         'Category',
-        null=True,
+        default='',
         blank=True,
-        on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+        on_delete=models.CASCADE)
+    sku = models.CharField(max_length=254, default='', blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6,
         decimal_places=2,
-        null=True,
+        default='',
         blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, default='', blank=True)
+    image = models.ImageField(default='', blank=True)
 
     def __str__(self):
         return self.name
@@ -50,7 +49,8 @@ class Review(models.Model):
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.user.username + '/ ' + self.product.name + '/ ' + self.updated_at
+        return self.user.username + '/ '
+        + self.product.name + '/ ' + self.updated_at
 
 
 class Event(models.Model):
@@ -60,9 +60,9 @@ class Event(models.Model):
     )
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
-    event_title = models.TextField(max_length=250, null=True, blank=True)
-    is_virtual = models.BooleanField(default=False, null=True, blank=True)
-    video_link = models.TextField(max_length=1000, null=True, blank=True)
+    event_title = models.TextField(max_length=250, default='', blank=True)
+    is_virtual = models.BooleanField(default=False,  blank=True)
+    video_link = models.TextField(max_length=1000, default='', blank=True)
 
     def __str__(self):
         return self.event_title + '/ ' + \
@@ -75,7 +75,7 @@ class Album(models.Model):
         on_delete=models.CASCADE
     )
     album_runtime = models.IntegerField(null=False, blank=False)
-    album_jacket = models.ImageField(null=True, blank=True)
+    album_jacket = models.ImageField(default='', blank=True)
 
     def __str__(self):
         return self.product.name
@@ -84,7 +84,7 @@ class Album(models.Model):
 class AlbumSongs(models.Model):
     album = models.ForeignKey(
         Album, on_delete=models.CASCADE, related_name='albumsongs')
-    song_title = models.TextField(max_length=250, null=True, blank=True)
+    song_title = models.TextField(max_length=250, default='', blank=True)
 
     def __str__(self):
         return self.album.product.name + '-' + self.song_title
