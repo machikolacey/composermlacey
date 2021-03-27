129c1ed0bded96484a9d2416a55e84043fdeff7c
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-27 21:21:36 +0000

    Fixed pep8 validations on products/ mlkshop/

--- mlkshop/settings.py
+++ mlkshop/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
-if os.path.exists('env.py'):
-    import env 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,16 +81,16 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
-          
+
             ],
             'builtins':[
-               'crispy_forms.templatetags.crispy_forms_tags',
-               'crispy_forms.templatetags.crispy_forms_field',
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
@@ -127,19 +125,16 @@ WSGI_APPLICATION = 'mlkshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-        DATABASES = {
-            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-
-
-
+    }
 
 
 # Password validation
@@ -193,7 +188,6 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
@@ -229,4 +223,3 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    
--- products/models.py
+++ products/models.py
@@ -3,12 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import User
 
 
-
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -20,16 +19,23 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
 
     def __str__(self):
         return self.name
@@ -43,41 +49,42 @@ class Review(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
-
     def __str__(self):
-        return self.user.username +'/ '+ self.product.name + '/ ' + self.updated_at
+        return self.user.username + '/ ' + self.product.name + '/ ' + self.updated_at
+
 
 class Event(models.Model):
     product = models.OneToOneField(
         Product,
         on_delete=models.CASCADE
-    )    
+    )
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
     event_title = models.TextField(max_length=250, null=True, blank=True)
     is_virtual = models.BooleanField(default=False, null=True, blank=True)
     video_link = models.TextField(max_length=1000, null=True, blank=True)
 
-
     def __str__(self):
-        return self.event_title+'/ '+ self.end_date_time.strftime("%m/%d/%Y, %H:%M:%S") 
-    
+        return self.event_title + '/ ' + \
+            self.end_date_time.strftime("%m/%d/%Y, %H:%M:%S")
+
 
 class Album(models.Model):
     product = models.OneToOneField(
         Product,
-        on_delete = models.CASCADE
+        on_delete=models.CASCADE
     )
     album_runtime = models.IntegerField(null=False, blank=False)
-    album_jacket =  models.ImageField(null=True, blank=True)
+    album_jacket = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.product.name 
+        return self.product.name
+
 
 class AlbumSongs(models.Model):
-     album = models.ForeignKey(
+    album = models.ForeignKey(
         Album, on_delete=models.CASCADE, related_name='albumsongs')
-     song_title = models.TextField(max_length=250, null=True, blank=True)
+    song_title = models.TextField(max_length=250, null=True, blank=True)
 
-     def __str__(self):
-        return self.album.product.name + '-' + self.song_title 
+    def __str__(self):
+        return self.album.product.name + '-' + self.song_title
