f0086abde21c746a52473fb2ed976c56bcfe5f87
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-27 22:47:47 +0000

    Fixed pep8 validation errors on products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,50 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('add/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('addreview/<int:product_id>/', views.add_review, name="add_review"),
-    path('editreview/<int:product_id>/<int:review_id>/', views.edit_review, name="edit_review"),
-    path('deletereview/<int:product_id>/<int:review_id>/', views.delete_review, name="delete_review"),
+    path(
+            '',
+            views.all_products,
+            name='products'
+        ),
+    path(
+            '<int:product_id>/',
+            views.product_detail,
+            name='product_detail'
+        ),
+    path(
+            'add/',
+            views.add_product,
+            name='add_product'
+        ),
+    path(
+            'edit/<int:product_id>/',
+            views.edit_product,
+            name='edit_product'
+        ),
+    path(
+            'delete/<int:product_id>/',
+            views.delete_product,
+            name='delete_product'
+         ),
+    path(
+            'delete/<int:product_id>/',
+            views.delete_product,
+            name='delete_product'
+        ),
+    path(
+            'addreview/<int:product_id>/',
+            views.add_review,
+            name="add_review"
+        ),
+    path(
+            'editreview/<int:product_id>/<int:review_id>/',
+            views.edit_review,
+            name="edit_review"
+        ),
+    path(
+            'deletereview/<int:product_id>/<int:review_id>/',
+            views.delete_review,
+            name="delete_review"
+        ),
 ]
