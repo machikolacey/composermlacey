728005a4ceca587abb858d7faa351616128afb70
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-27 00:02:06 +0000

    fixed pep8 validation issues on about/views.py

--- about/views.py
+++ about/views.py
@@ -2,25 +2,22 @@ from django.shortcuts import render
 from .models import Page
 from .models import SampleVideo
 from .models import Images
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request, permalink):
     """ A view to return the index page """
 
-
     sampleVideos = SampleVideo.objects.all()
-
 
     for index, video in enumerate(sampleVideos, start=0):
         embedvideo = video.youtube_link.replace("watch?v=", "embed/")
         sampleVideos[index].embedvideo = embedvideo
 
-
     try:
         page = Page.objects.get(permalink=permalink)
     except ObjectDoesNotExist:
         page = None
-
 
     images = Images.objects.filter(page=page)
 
@@ -29,13 +26,9 @@ def index(request, permalink):
     else:
         template = 'advert/page.html'
 
-
-
-
     context = {
         'page': page,
         'sampleVideos': sampleVideos,
         'images': images
     }
     return render(request, template, context)
-
