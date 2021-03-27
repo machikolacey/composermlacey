ce26011d6dd6b389d545f264dbdbe5cd8f2a309b
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-27 21:48:06 +0000

    Rolled back changes on products/models.py

--- products/models.py
+++ products/models.py
@@ -20,21 +20,21 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         'Category',
-        default='',
+        null=True,
         blank=True,
-        on_delete=models.CASCADE)
-    sku = models.CharField(max_length=254, default='', blank=True)
+        on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6,
         decimal_places=2,
-        default='',
+        null=True,
         blank=True)
-    image_url = models.URLField(max_length=1024, default='', blank=True)
-    image = models.ImageField(default='', blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -49,8 +49,7 @@ class Review(models.Model):
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.user.username + '/ '
-        + self.product.name + '/ ' + self.updated_at
+        return self.user.username + '/ ' + self.product.name + '/ ' + self.updated_at
 
 
 class Event(models.Model):
@@ -60,9 +59,9 @@ class Event(models.Model):
     )
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
-    event_title = models.TextField(max_length=250, default='', blank=True)
-    is_virtual = models.BooleanField(default=False,  blank=True)
-    video_link = models.TextField(max_length=1000, default='', blank=True)
+    event_title = models.TextField(max_length=250, null=True, blank=True)
+    is_virtual = models.BooleanField(default=False, null=True, blank=True)
+    video_link = models.TextField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.event_title + '/ ' + \
@@ -75,7 +74,7 @@ class Album(models.Model):
         on_delete=models.CASCADE
     )
     album_runtime = models.IntegerField(null=False, blank=False)
-    album_jacket = models.ImageField(default='', blank=True)
+    album_jacket = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.product.name
@@ -84,7 +83,7 @@ class Album(models.Model):
 class AlbumSongs(models.Model):
     album = models.ForeignKey(
         Album, on_delete=models.CASCADE, related_name='albumsongs')
-    song_title = models.TextField(max_length=250, default='', blank=True)
+    song_title = models.TextField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.album.product.name + '-' + self.song_title
