9aee1090937bfccf9a75b5a44d341b3ebf65d8df
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-29 22:51:07 +0000

    Changed var to let on checkout.js

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,16 +1,16 @@
  // Update quantity on click
     $('.update-link').click(function(e) {
-        var form = $(this).prev('.update-form');
+        const form = $(this).prev('.update-form');
         form.submit();
     })
 
     // Remove item and reload on click
     $('.remove-item').click(function(e) {
-        var csrfToken = "{{ csrf_token }}";
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var size = $(this).data('product_size');
-        var url = `/bag/remove/${itemId}/`;
-        var data = {'csrfmiddlewaretoken': csrfToken, 'product_size': size};
+        const csrfToken = "{{ csrf_token }}";
+        const itemId = $(this).attr('id').split('remove_')[1];
+        const size = $(this).data('product_size');
+        const url = `/bag/remove/${itemId}/`;
+        const data = {'csrfmiddlewaretoken': csrfToken, 'product_size': size};
 
         $.post(url, data)
          .done(function() {
