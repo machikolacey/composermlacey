5416c7f59ebf833b3ccfa7fada2de941bea360ea
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-03-31 03:29:04 +0000

    Added back to top button on base.html

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+
     $('.toast').toast('show');
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $("#toTop").click(function() {
+        console.log('clicked');
+        $("body, html").animate({scrollTop: 0}, 1000);
+    });
+
+ });
